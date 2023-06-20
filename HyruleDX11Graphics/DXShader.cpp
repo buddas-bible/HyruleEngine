f3012e40bf80993d549c17a5b28658af9c1d7cb4
buddas-bible <kh951220@gmail.com>
2023-06-21 01:18:23 +0900

    커밋 안된 코드 커밋

--- HyruleDX11Graphics/DXShader.cpp
+++ HyruleDX11Graphics/DXShader.cpp
@@ -14,9 +14,12 @@ DXShader::~DXShader()
 int DXShader::CreateVertexShader()
 {
 	// m_device->GetDeviceContext()->IAGetVertexBuffers;
+
+	return 0;
 }
 
 int DXShader::CreatePixelShader()
 {
 
+	return 0;
 }
