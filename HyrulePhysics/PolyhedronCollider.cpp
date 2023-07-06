1479e0ddad4ede33f39a8e66b42e44f55efb87e9
Myname <MyEmail@a.a>
2023-07-06 21:56:23 +0900

    ㅇㅇ

---
+++ HyrulePhysics/PolyhedronCollider.cpp
@@ -0,0 +1,1 @@
+#include "PolyhedronCollider.h"
---
+++ HyrulePhysics/PolyhedronCollider.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Collider.h"
+
+namespace Hyrule
+{
+	namespace Physics
+	{
+		class PolyhedronCollider : public Collider
+		{
+		};
+	}
+}
