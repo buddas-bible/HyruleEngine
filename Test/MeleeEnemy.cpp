14ac353aec0ce5bfeff4de6923a7dc6e5f1d5024
buddas-bible <kh951220@gmail.com>
2023-08-16 02:40:52 +0900

    덜 커밋된거

--- Test/MeleeEnemy.cpp
+++ Test/MeleeEnemy.cpp
@@ -14,12 +14,15 @@ namespace Hyrule
 		gameObject->GetTransform()->GetChild(0);
 
 		// 플레이어를 향해서 공격을 실행시켜야 하는데...
+		// 공격 콜라이더 생성 했다가 끄고 그런게 있어야하나
 
 		return EnemyState::DAMAGED;
 	}
 
 	EnemyState MeleeEnemy::Damaged()
 	{
+
+
 		return EnemyState::DAMAGED;
 	}
 
